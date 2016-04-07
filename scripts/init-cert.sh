80f794dd0555064bd4ab03cdd21d46efa221fa39
Shinichiro Aska <s.aska.org@gmail.com>
2016-04-07 18:29:29 +0900

    Update readme

--- scripts/init-cert.sh
+++ scripts/init-cert.sh
@@ -5,14 +5,22 @@
 # sudo git clone https://github.com/letsencrypt/letsencrypt
 # /usr/local/src/letsencrypt/letsencrypt-auto --help
 #
-# * generate ssl_dhparam
+# * Generate ssl_dhparam
 # sudo openssl dhparam 2048 -out /etc/letsencrypt/live/justaway.info/dhparam.pem
 #
-# * generate ssl_session_ticket_key
+# * Generate ssl_session_ticket_key
 # sudo openssl rand 48 > ticket.key
 # sudo mv ticket.key /etc/letsencrypt/live/justaway.info/
 #
-# * ct submit
+# * Install st submit
+# cd /usr/local/src
+# sudo wget https://github.com/grahamedgecombe/ct-submit/archive/master.tar.gz
+# sudo tar zxvf master.tar.gz
+# cd ct-submit-master/
+# sudo /usr/local/go/bin/go build
+# sudo install -s -m755 ./ct-submit-master /usr/local/bin/ct-submit
+#
+# * Run ct submit
 # sudo sh /home/aska/justaway.info/scripts/ct-submit.sh
 
 /usr/local/src/letsencrypt/letsencrypt-auto certonly --webroot -d justaway.info --webroot-path /home/aska/justaway.info/static -m s.aska.org@gmail.com --agree-tos
