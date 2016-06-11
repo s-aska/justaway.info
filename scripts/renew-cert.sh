d56a44ba961381e0951573fb201494627cf11da4
Shinichiro Aska <s.aska.org@gmail.com>
2016-06-11 16:26:28 +0900

    Add /etc/init.d/nginx reload

--- scripts/renew-cert.sh
+++ scripts/renew-cert.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# crontab
-# 0 4 8 * * /home/aska/justaway.info/scripts/renew-cert.sh
+# crontab (root)
+# 0 5 1 * * /home/aska/justaway.info/scripts/renew-cert.sh
 
 /usr/local/src/letsencrypt/letsencrypt-auto certonly --webroot -d justaway.info --webroot-path /home/aska/justaway.info/static --renew-by-default
 /home/aska/justaway.info/scripts/ct-submit.sh
+/etc/init.d/nginx reload
