74951d1720228e7ec8d31edbe80ac2894815f4e0
Shinichiro Aska <s.aska.org@gmail.com>
2017-07-21 19:02:40 +0900

    Netlify

--- scripts/renew-cert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# crontab (root)
-# 0 5 1 * * /home/aska/justaway.info/scripts/renew-cert.sh
-
-/usr/local/src/letsencrypt/letsencrypt-auto certonly --webroot -d justaway.info --webroot-path /home/aska/justaway.info/static --renew-by-default
-/home/aska/justaway.info/scripts/ct-submit.sh
-/etc/init.d/nginx reload
